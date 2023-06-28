4ba968671344e2b98a6e850cb2c757f1c8b7005e
raphaelohai5 <sameleno6@gmail.com>
2023-06-28 17:48:58 +0100

    more fixes

--- src/components/works/myWork.jsx
+++ src/components/works/myWork.jsx
@@ -33,7 +33,15 @@ const MyWork = () => {
                 e-commerce. Here, small businesses can create, customize and
                 manage their personal e-commerce stores as well as the logistics
                 involved, all for free, via their smart devices`}
-                Tools={["MongoDB", "Node", "React", "Redux", "Express", "linux", "Apache2"]}
+                Tools={[
+                  "MongoDB",
+                  "Node",
+                  "React",
+                  "Redux",
+                  "Express",
+                  "linux",
+                  "Apache2",
+                ]}
                 Image={"/fingertipps.png"}
                 overlayId={"fff"}
                 noCode={true}
@@ -42,10 +50,7 @@ const MyWork = () => {
               />
               <RightLeftShowcase
                 projectName={"cowrywise clone"}
-                discription={`Fingertipps, is an all-in-one platform that facilitates
-                e-commerce. Here, small businesses can create, customize and
-                manage their personal e-commerce stores as well as the logistics
-                involved, all for free, via their smart devices`}
+                discription={`I found the design of CowryWise to be highly impressive, which has motivated me to create a similar landing page as an homage to their aesthetic and user experience.`}
                 Tools={["React", "Redux", "CSS"]}
                 Image={"/cowryWise.png"}
                 overlayId={"fff22"}
@@ -56,10 +61,7 @@ const MyWork = () => {
 
               <LeftRightShowcase
                 projectName={"GitHub Clone"}
-                discription={`Fingertipps, is an all-in-one platform that facilitates
-                e-commerce. Here, small businesses can create, customize and
-                manage their personal e-commerce stores as well as the logistics
-                involved, all for free, via their smart devices`}
+                discription={`I have always been fascinated by the visually appealing design of the GitHub landing page, which has motivated me to create a clone of it as a demonstration of its impressive aesthetics`}
                 Tools={["React", "Redux", "CSS", "Material-UI"]}
                 Image={"/GitHub.png"}
                 overlayId={"fdeff22"}
@@ -69,11 +71,8 @@ const MyWork = () => {
               />
 
               <RightLeftShowcase
-                projectName={"cowrywise clone"}
-                discription={`Fingertipps, is an all-in-one platform that facilitates
-                e-commerce. Here, small businesses can create, customize and
-                manage their personal e-commerce stores as well as the logistics
-                involved, all for free, via their smart devices`}
+                projectName={"Meta's Quest pro"}
+                discription={`I utilized the Apple Design System to redesign and develop the landing page of Meta’s Quest Pro, incorporating an engaging video scroll effect as an additional enhancement`}
                 Tools={["React", "Redux", "Css"]}
                 Image={"/metaQuest.png"}
                 overlayId={"fdeededf22"}
@@ -84,10 +83,7 @@ const MyWork = () => {
 
               <LeftRightShowcase
                 projectName={"GitHub Clone"}
-                discription={`Fingertipps, is an all-in-one platform that facilitates
-                e-commerce. Here, small businesses can create, customize and
-                manage their personal e-commerce stores as well as the logistics
-                involved, all for free, via their smart devices`}
+                discription={`I undertook the redesign and development of Jack Daniel's landing page, incorporating captivating SVG path animations and a seamless video scroll animation to enhance its visual appeal and user experience.`}
                 Tools={["React (TypeScript)", "CSS", "Figma"]}
                 Image={"/jack.png"}
                 overlayId={"fdefdcdc22"}
