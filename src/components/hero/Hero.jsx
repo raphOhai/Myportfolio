e9c9ddbb475f7d93573fdbf7daffccf47ab6dd74
raphaelohai5 <sameleno6@gmail.com>
2023-08-02 00:31:29 +0100

    fixed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             style={{ marginTop: "4rem" }}
             className="flex gap2 Start SlideUp2 group"
           >
-            <a href="https://drive.google.com/file/d/10Mu3BXWuxC-KiuCzbxtVBPG_zL8O5NvT/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/10NZVMFAomk6esCI4_o-cA7BrRubirNEY/view?usp=sharing">
               <Button
                 slide={"btnslfffd45"}
                 text={"btffntxt45"}
